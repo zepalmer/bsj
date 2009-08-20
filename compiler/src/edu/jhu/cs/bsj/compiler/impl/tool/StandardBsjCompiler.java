0d54143b9379dbae4327487fb4745a9fd716d28c
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2009-08-20 16:29:44 +0000

    Added project package structure.

---
+++ compiler/src/edu/jhu/cs/bsj/compiler/impl/tool/StandardBsjCompiler.java
@@ -0,0 +1,12 @@
+package edu.jhu.cs.bsj.compiler.impl.tool;
+
+import edu.jhu.cs.bsj.compiler.tool.BsjCompiler;
+
+/**
+ * This class is a standard implementation of the BSJ compiler.
+ * @author Zachary Palmer
+ */
+public class StandardBsjCompiler implements BsjCompiler
+{
+
+}
---
+++ compiler/src/edu/jhu/cs/bsj/compiler/tool/BsjCompiler.java
@@ -0,0 +1,11 @@
+package edu.jhu.cs.bsj.compiler.tool;
+
+/**
+ * This interface is implemented by any class which can serve as a BSJ compiler.
+ * 
+ * @author Zachary Palmer
+ */
+public interface BsjCompiler
+{
+
+}
