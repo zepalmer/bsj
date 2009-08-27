a53d31f0bf813cacfdf136771dcdfe67d0a11e9a
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2009-08-27 21:43:36 +0000

    Disabled node supplement; this is not the way to handle our visitor pattern.

--- compiler/utils/data/srcgen/supplement/Node.java
+++ compiler/utils/data/srcgen/supplement/Node.java
@@ -1,13 +1,13 @@
-public class Node
-{
-	/* GEN:methodheader */
-    /**
-	 * TODO
-	 */
-	public void accept(BsjNodeVisitor visitor)
-	/* GEN:methodbody */
-	{
-		// TODO
-	}
-	/* GEN:stop */
-}
+//public class Node
+//{
+//	/* GEN:methodheader */
+//    /**
+//	 * TODO
+//	 */
+//	public void accept(BsjNodeVisitor visitor)
+//	/* GEN:methodbody */
+//	{
+//		// TODO
+//	}
+//	/* GEN:stop */
+//}
