fa6c84da965efa5b82de4e3deccbd1a35f885e1f
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-01-06 20:40:18 +0000

    Added src-pre directory for sources on which the BSJ ANTLR parser can depend.

---
+++ compiler/parser/src-pre/edu/jhu/cs/bsj/compiler/tool/parser/BsjAntlrParserUtils.java
@@ -0,0 +1,72 @@
+package edu.jhu.cs.bsj.compiler.tool.parser;
+
+public class BsjAntlrParserUtils
+{
+    /**
+     * Unescapes the provided string.  Textual escape codes are replaced with the characters they represent.
+     * @param string The string to unescape.
+     * @return The unescaped string.
+     */
+    public static final String unescape(String string)
+    {
+        char[] chars = string.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < chars.length)
+        {
+            if (chars[i] == '\\')
+            {
+                i++;
+                if (chars[i] >= '0' && chars[i] <= '9')
+                {
+                    StringBuilder nums = new StringBuilder();
+                    while (nums.length() < 3 && i < chars.length - 1 &&  chars[i] >= '0' && chars[i] <= '9')
+                    {
+                        nums.append(chars[i]);
+                        i++;
+                    }
+                    // We don't need to bounds check - no one will pass us illegal escape codes
+                    char c = (char)(Integer.parseInt(nums.toString()));
+                    sb.append(c);
+                } else
+                {
+                    switch (chars[i])
+                    {
+                        case 'b':
+                            sb.append('\b');
+                            break;
+                        case 't':
+                            sb.append('\t');
+                            break;
+                        case 'n':
+                            sb.append('\n');
+                            break;
+                        case 'f':
+                            sb.append('\f');
+                            break;
+                        case 'r':
+                            sb.append('\r');
+                            break;
+                        case '"':
+                            sb.append('"');
+                            break;
+                        case '\'':
+                            sb.append('\'');
+                            break;
+                        case '\\':
+                            sb.append('\\');
+                            break;
+                        default:
+                            // TODO: handle and report error: illegal escape character
+                    }
+                    i++;
+                }
+            } else
+            {
+                sb.append(chars[i]);
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
