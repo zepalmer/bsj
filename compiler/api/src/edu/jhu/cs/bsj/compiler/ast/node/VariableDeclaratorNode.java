050d9e727c24bef0cdc11888ee7cc3826f270fee
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-01-06 18:38:50 +0000

    Added a comment to VariableDeclaratorNode.

--- compiler/api/src/edu/jhu/cs/bsj/compiler/ast/node/VariableDeclaratorNode.java
+++ compiler/api/src/edu/jhu/cs/bsj/compiler/ast/node/VariableDeclaratorNode.java
@@ -10,7 +10,8 @@ package edu.jhu.cs.bsj.compiler.ast.node;
  * <pre>y</pre>
  * .  Note that variable multi-declaration syntax allows different types, as in
  * <pre>int x = 5, y[] = new int[2];</pre>
- * Hence, type exists on this node instead of its parent.
+ * Hence, type exists on this node instead of its parent.  If the variable is not initialized, the
+ * <tt>initializer</tt> field is <tt>null</tt>.
  */
 public interface VariableDeclaratorNode extends StatementNode
 {
