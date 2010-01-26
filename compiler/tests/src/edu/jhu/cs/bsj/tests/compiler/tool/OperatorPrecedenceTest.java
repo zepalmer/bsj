64a2da324024feffb8e986393352078ee8169076
Joseph Riley <Joseph Riley@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-01-26 21:32:32 +0000

    Testing for operator precedence.

--- compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/OperatorPrecedenceTest.java
+++ compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/OperatorPrecedenceTest.java
@@ -1,13 +1,213 @@
 package edu.jhu.cs.bsj.tests.compiler.tool;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import edu.jhu.cs.bsj.compiler.ast.AssignmentOperator;
+import edu.jhu.cs.bsj.compiler.ast.BinaryOperator;
+import edu.jhu.cs.bsj.compiler.ast.node.Node;
+import edu.jhu.cs.bsj.compiler.impl.ast.BsjNodeFactoryImpl;
+import edu.jhu.cs.bsj.compiler.impl.tool.BsjSourceSerializerImpl;
 
 
 public class OperatorPrecedenceTest
 {
     @Test
-    public void testBinaryOperator()
+    public void testBinaryOperatorMultiplicativeAndAdditive()
     {
-        
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.MINUS), 
+                BinaryOperator.DIVIDE);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 / (5 - 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorAdditiveAndShift()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.LEFT_SHIFT), 
+                BinaryOperator.PLUS);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 + (5 << 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorShiftAndGreaterThan()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.GREATER_THAN), 
+                BinaryOperator.LEFT_SHIFT);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 << (5 > 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorGreaterThanAndEqual()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.EQUAL), 
+                BinaryOperator.GREATER_THAN);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 > (5 == 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorEqualAndLogicalAnd()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.LOGICAL_AND), 
+                BinaryOperator.EQUAL);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 == (5 & 5)");        
+    }    
+    
+    @Test
+    public void testBinaryOperatorLogicalAndAndXor()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.XOR), 
+                BinaryOperator.LOGICAL_AND);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 & (5 ^ 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorXorAndLogicalOr()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.LOGICAL_OR), 
+                BinaryOperator.XOR);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 ^ (5 | 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorLogicalOrAndConditionalAnd()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.CONDITIONAL_AND), 
+                BinaryOperator.LOGICAL_OR);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 | (5 && 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorConditionalAndAndConditionalOr()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeBinaryExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        BinaryOperator.CONDITIONAL_OR), 
+                BinaryOperator.CONDITIONAL_AND);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 && (5 || 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorAndConditionalExpression()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeConditionalExpressionNode(
+                        factory.makeIntLiteralNode(5),
+                        factory.makeIntLiteralNode(5), 
+                        factory.makeIntLiteralNode(5)), 
+                BinaryOperator.CONDITIONAL_AND);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 && (5 ? 5 : 5)");        
+    }
+    
+    @Test
+    public void testBinaryOperatorAndAssignment()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeBinaryExpressionNode(
+                factory.makeIntLiteralNode(5),
+                factory.makeAssignmentNode(
+                        factory.makeIntLiteralNode(5),
+                        AssignmentOperator.MODULUS_ASSIGNMENT, 
+                        factory.makeIntLiteralNode(5)), 
+                BinaryOperator.CONDITIONAL_AND);
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "5 && (5 %= 5)");        
+    }
+    
+    @Test
+    public void testConditionalExpressionAndAssignment()
+    {
+        BsjNodeFactoryImpl factory = new BsjNodeFactoryImpl();
+        Node node = factory.makeConditionalExpressionNode(
+                factory.makeAssignmentNode(
+                        factory.makeIntLiteralNode(5),
+                        AssignmentOperator.MODULUS_ASSIGNMENT, 
+                        factory.makeIntLiteralNode(5)),
+                        factory.makeAssignmentNode(
+                                factory.makeIntLiteralNode(5),
+                                AssignmentOperator.AND_ASSIGNMENT, 
+                                factory.makeIntLiteralNode(5)), 
+                                factory.makeAssignmentNode(
+                                        factory.makeIntLiteralNode(5),
+                                        AssignmentOperator.DIVIDE_ASSIGNMENT, 
+                                        factory.makeIntLiteralNode(5))); 
+        assertEquals(
+                node.executeOperation(new BsjSourceSerializerImpl(), null),
+                "(5 %= 5) ? (5 &= 5) : (5 /= 5)");        
     }
 }
