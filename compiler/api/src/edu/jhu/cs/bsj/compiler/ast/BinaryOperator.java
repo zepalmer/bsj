8d9c7d1aade53183a9347d6096ec6a319134bab8
Joseph Riley <Joseph Riley@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-01-26 20:05:56 +0000

    Adding precedence to binary operators.

--- compiler/api/src/edu/jhu/cs/bsj/compiler/ast/BinaryOperator.java
+++ compiler/api/src/edu/jhu/cs/bsj/compiler/ast/BinaryOperator.java
@@ -1,29 +1,81 @@
 package edu.jhu.cs.bsj.compiler.ast;
 
+import java.util.Comparator;
+
 public enum BinaryOperator
 {
-	LOGICAL_AND,            // &
-	LOGICAL_OR,             // |
-	XOR,                    // ^
+    DIVIDE(9),                 // /
+    MODULUS(9),                // %
+    MULTIPLY(9),               // *
 
-	CONDITIONAL_AND,        // &&
-	CONDITIONAL_OR,         // ||
+    MINUS(8),                  // -
+    PLUS(8),                   // +
 
-	DIVIDE,                 // /
-	MINUS,                  // -
-	MODULUS,                // %
-	MULTIPLY,               // *
-	PLUS,                   // +
+    LEFT_SHIFT(7),             // <<
+    RIGHT_SHIFT(7),            // >>
+    UNSIGNED_RIGHT_SHIFT(7),   // >>>
 
-	EQUAL,                  // ==
-	NOT_EQUAL,              // !=
+    GREATER_THAN(6),           // >
+    GREATER_THAN_EQUAL(6),     // >=
+    LESS_THAN(6),              // <
+    LESS_THAN_EQUAL(6),        // <=
+
+    EQUAL(5),                  // ==
+    NOT_EQUAL(5),              // !=
+    
+    LOGICAL_AND(4),            // &
+
+    XOR(3),                    // ^
+    
+    LOGICAL_OR(2),             // |
+
+	CONDITIONAL_AND(1),        // &&
 	
-	GREATER_THAN,           // >
-	GREATER_THAN_EQUAL,     // >=
-	LESS_THAN,              // <
-	LESS_THAN_EQUAL,        // <=
-
-	LEFT_SHIFT,             // <<
-	RIGHT_SHIFT,            // >>
-	UNSIGNED_RIGHT_SHIFT    // >>>
+	CONDITIONAL_OR(0)          // ||
+	
+	;
+	
+    /**
+     * The precedence for this operation. 
+     */
+	private int precedence;
+	
+	/**
+	 * Constructor.
+	 * @param precedence the precedence for this operation.
+	 */
+	private BinaryOperator(int precedence)
+	{
+	    this.precedence = precedence;
+	}
+	
+	/**
+	 * Compares two binary operators in terms of their precedence.
+	 */
+	public class PrecedenceComparator implements Comparator<BinaryOperator>
+	{
+	    /**
+	     * Compares two binary operators in terms of their precedence.
+	     * @param a the first operator.
+	     * @param b the second operator.
+	     * @return -1, 0, or 1 if the first operator is less than, equal to,
+	     *     or greater than the second in terms of precedence. 
+	     */
+        @Override
+        public int compare(BinaryOperator a, BinaryOperator b)
+        {
+            if (a.precedence < b.precedence)
+            {
+                return -1;
+            }
+            else if (a.precedence > b.precedence)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }	    
+	}	
 }
