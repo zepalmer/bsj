1f396a8faa9109aa6ba0efcb9a63960656642f50
jwriley <jwriley@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-02-03 18:15:42 +0000

    Tests for lifter.

--- compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/compiler/BsjTreeLifterTest.java
+++ compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/compiler/BsjTreeLifterTest.java
@@ -9,11 +9,33 @@ import java.util.List;
 
 import org.junit.Test;
 
+import edu.jhu.cs.bsj.compiler.ast.AccessModifier;
 import edu.jhu.cs.bsj.compiler.ast.BsjNodeFactory;
 import edu.jhu.cs.bsj.compiler.ast.NameCategory;
+import edu.jhu.cs.bsj.compiler.ast.PrimitiveType;
+import edu.jhu.cs.bsj.compiler.ast.node.AnnotationNode;
+import edu.jhu.cs.bsj.compiler.ast.node.BlockNode;
 import edu.jhu.cs.bsj.compiler.ast.node.BlockStatementNode;
+import edu.jhu.cs.bsj.compiler.ast.node.ClassBodyNode;
+import edu.jhu.cs.bsj.compiler.ast.node.ClassDeclarationNode;
+import edu.jhu.cs.bsj.compiler.ast.node.ClassMemberNode;
+import edu.jhu.cs.bsj.compiler.ast.node.ClassModifiersNode;
+import edu.jhu.cs.bsj.compiler.ast.node.CompilationUnitNode;
 import edu.jhu.cs.bsj.compiler.ast.node.ExpressionNode;
+import edu.jhu.cs.bsj.compiler.ast.node.IdentifierNode;
+import edu.jhu.cs.bsj.compiler.ast.node.ImportNode;
+import edu.jhu.cs.bsj.compiler.ast.node.IntLiteralNode;
+import edu.jhu.cs.bsj.compiler.ast.node.ListNode;
+import edu.jhu.cs.bsj.compiler.ast.node.MethodDeclarationNode;
+import edu.jhu.cs.bsj.compiler.ast.node.MethodModifiersNode;
 import edu.jhu.cs.bsj.compiler.ast.node.Node;
+import edu.jhu.cs.bsj.compiler.ast.node.PrimitiveTypeNode;
+import edu.jhu.cs.bsj.compiler.ast.node.ReturnNode;
+import edu.jhu.cs.bsj.compiler.ast.node.TypeDeclarationNode;
+import edu.jhu.cs.bsj.compiler.ast.node.TypeNode;
+import edu.jhu.cs.bsj.compiler.ast.node.TypeParameterNode;
+import edu.jhu.cs.bsj.compiler.ast.node.UnparameterizedTypeNode;
+import edu.jhu.cs.bsj.compiler.ast.node.VariableNode;
 import edu.jhu.cs.bsj.compiler.impl.ast.BsjNodeFactoryImpl;
 import edu.jhu.cs.bsj.compiler.impl.tool.compiler.BsjTreeLifter;
 import edu.jhu.cs.bsj.compiler.impl.tool.serializer.BsjSourceSerializerImpl;
@@ -35,15 +57,15 @@ public class BsjTreeLifterTest
     {
         BsjNodeFactory factory = new BsjNodeFactoryImpl();
         BsjTreeLifter treeLifter = new BsjTreeLifter(factory);       
+        BsjParserImpl parser = new BsjParserImpl(new BsjNodeFactoryImpl());
         
         String baseCode = 
                 "public class BaseClass " + 
                 "{" + 
-                    "public void foo()" + 
-                    "{int x;}" + 
+                    "public int foo()" + 
+                    "{return 666;}" + 
                 "}";
-
-        BsjParserImpl parser = new BsjParserImpl(new BsjNodeFactoryImpl());
+        
         Node ast = null;
         try
         {
@@ -54,16 +76,94 @@ public class BsjTreeLifterTest
             fail(e.getMessage());
         }
         
-        Node metaNode = factory.makeContinueNode(factory.makeIdentifierNode("testing"));
         List<BlockStatementNode>list = new ArrayList<BlockStatementNode>();
         
         ExpressionNode metaFactory = factory.makeFieldAccessByNameNode(factory.makeSimpleNameNode(factory.makeIdentifierNode("factory"), NameCategory.TYPE));
-        ast.executeOperation(treeLifter, new Pair<ExpressionNode, List<BlockStatementNode>>(metaFactory, list));
         
-        Node block = factory.makeBlockNode(factory.makeListNode(list));
+        String varName = ast.executeOperation(treeLifter, new Pair<ExpressionNode, List<BlockStatementNode>>(metaFactory, list));
+        System.out.println(varName);
         
+        Node block = factory.makeBlockNode(factory.makeListNode(list));        
         String regen2 = block.executeOperation(new BsjSourceSerializerImpl(), null);
         System.out.println(regen2);
 
+        //=============================================REMOVE
+		List<ImportNode> v0 = new ArrayList<ImportNode>();
+		ListNode<ImportNode> v1 = (factory).makeListNode(v0);
+		List<TypeDeclarationNode> v2 = new ArrayList<TypeDeclarationNode>();
+		List<AnnotationNode> v3 = new ArrayList<AnnotationNode>();
+		ListNode<AnnotationNode> v4 = (factory).makeListNode(v3);
+		ClassModifiersNode v5 = (factory).makeClassModifiersNode(
+				AccessModifier.PUBLIC, false, false, false, false, v4);
+		List<TypeNode> v6 = new ArrayList<TypeNode>();
+		ListNode<TypeNode> v7 = (factory).makeListNode(v6);
+		List<ClassMemberNode> v8 = new ArrayList<ClassMemberNode>();
+		List<BlockStatementNode> v9 = new ArrayList<BlockStatementNode>();
+		IntLiteralNode v10 = (factory).makeIntLiteralNode(666);
+		ReturnNode v11 = (factory).makeReturnNode(v10);
+		v9.add(v11);
+		ListNode<BlockStatementNode> v12 = (factory).makeListNode(v9);
+		BlockNode v13 = (factory).makeBlockNode(v12);
+		List<AnnotationNode> v14 = new ArrayList<AnnotationNode>();
+		ListNode<AnnotationNode> v15 = (factory).makeListNode(v14);
+		MethodModifiersNode v16 = (factory).makeMethodModifiersNode(
+				AccessModifier.PUBLIC, false, false, false, false, false,
+				false, v15);
+		IdentifierNode v17 = (factory).makeIdentifierNode("foo");
+		List<VariableNode> v18 = new ArrayList<VariableNode>();
+		ListNode<VariableNode> v19 = (factory).makeListNode(v18);
+		PrimitiveTypeNode v20 = (factory)
+				.makePrimitiveTypeNode(PrimitiveType.INT);
+		List<UnparameterizedTypeNode> v21 = new ArrayList<UnparameterizedTypeNode>();
+		ListNode<UnparameterizedTypeNode> v22 = (factory).makeListNode(v21);
+		List<TypeParameterNode> v23 = new ArrayList<TypeParameterNode>();
+		ListNode<TypeParameterNode> v24 = (factory).makeListNode(v23);
+		MethodDeclarationNode v25 = (factory).makeMethodDeclarationNode(v13,
+				v16, v17, v19, null, v20, v22, v24, null);
+		v8.add(v25);
+		ListNode<ClassMemberNode> v26 = (factory).makeListNode(v8);
+		ClassBodyNode v27 = (factory).makeClassBodyNode(v26);
+		List<TypeParameterNode> v28 = new ArrayList<TypeParameterNode>();
+		ListNode<TypeParameterNode> v29 = (factory).makeListNode(v28);
+		IdentifierNode v30 = (factory).makeIdentifierNode("BaseClass");
+		ClassDeclarationNode v31 = (factory).makeClassDeclarationNode(v5, null,
+				v7, v27, v29, v30, null);
+		v2.add(v31);
+		ListNode<TypeDeclarationNode> v32 = (factory).makeListNode(v2);
+		CompilationUnitNode v33 = (factory).makeCompilationUnitNode(null, v1,
+				v32);
+		String blah = v33.executeOperation(new BsjSourceSerializerImpl(), null);
+		System.out.println("===============================================");
+        System.out.println(blah);
+        //=============================================REMOVE
+		
+	}
+    
+    /**
+     * Compiles and runs a block of code which generates a lifted AST.
+     * @param varName
+     * @param code
+     * @return the lifted AST.
+     */
+    public CompilationUnitNode compileMeta(String varName, BlockNode code)
+    {
+        String wrapperCode = 
+            "public class BaseClass " + 
+            "{" + 
+                "public int foo()" + 
+                "{return 666;}" + 
+            "}";
+        
+    	return null;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
