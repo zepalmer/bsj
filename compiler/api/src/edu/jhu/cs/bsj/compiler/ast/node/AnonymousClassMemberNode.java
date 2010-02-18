d340d7fb627161e9e7afb4a943ddc26397cbaf15
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-02-18 01:04:55 +0000

    Corrected broken @link in Javadocs.

--- compiler/api/src/edu/jhu/cs/bsj/compiler/ast/node/AnonymousClassMemberNode.java
+++ compiler/api/src/edu/jhu/cs/bsj/compiler/ast/node/AnonymousClassMemberNode.java
@@ -7,7 +7,7 @@ import edu.jhu.cs.bsj.compiler.ast.BsjNodeFactory;
 /**
  * This tagging interface is used to denote AST nodes which can be used as members of an anonymous class's body.
  * Since the set of declarations which may appear in an anonymous class is a subset of that which may appear in a
- * named class, this type is a subtype of {@link ClassMember}.
+ * named class, this type is a subtype of {@link ClassMemberNode}.
  */
 @Generated(value={"edu.jhu.cs.bsj.compiler.utils.generator.SourceGenerator"})
 public interface AnonymousClassMemberNode extends Node, ClassMemberNode
