e0b6edc92a293c2b851aef2261bd559f71f2a4ee
jwriley <jwriley@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-02-19 22:50:51 +0000

    Work continuing on bsjc, added apache commons-cli for command line parsing.

--- compiler/generator/src/edu/jhu/cs/bsj/compiler/impl/tool/bsjc/BsjC.java
+++ compiler/generator/src/edu/jhu/cs/bsj/compiler/impl/tool/bsjc/BsjC.java
@@ -3,7 +3,7 @@ package edu.jhu.cs.bsj.compiler.impl.tool.bsjc;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +11,14 @@ import java.util.Map;
 import javax.tools.DiagnosticListener;
 import javax.tools.JavaFileObject;
 import javax.tools.JavaFileObject.Kind;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+import com.sun.corba.se.spi.ior.MakeImmutable;
 
 import edu.jhu.cs.bsj.compiler.impl.tool.compiler.StandardBsjCompiler;
 import edu.jhu.cs.bsj.compiler.impl.tool.filemanager.BsjCompilerLocation;
@@ -32,7 +40,7 @@ public class BsjC
 	/**
 	 * The {@link BsjFileManager} used for the filesystem abstraction.
 	 */
-	public BsjFileManager bsjFileManager;
+	private BsjFileManager bsjFileManager;
 	
 	/**
 	 * Diagnostic listener for compilation.
@@ -42,7 +50,7 @@ public class BsjC
 	/**
 	 * Objects to be compiled.
 	 */
-	public List<BsjFileObject> compileObjects;
+	private List<BsjFileObject> compileObjects;
 	
 	/**
 	 * Constructor.
@@ -61,6 +69,23 @@ public class BsjC
 	 */
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
+		CommandLineParser parser = new PosixParser();
+		CommandLine cmd = null;
+		try
+		{
+			cmd = parser.parse(BsjC.initOptions(), args);
+		} catch (ParseException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+
+		
+		
+		
+		
 		// TODO Auto-generated method stub
 		Map<BsjCompilerLocation, LocationManager> map = new HashMap<BsjCompilerLocation, LocationManager>();
 		map.put(BsjCompilerLocation.SOURCE_PATH, new RegularFileLocationManager(null, new File(".")));//src
@@ -78,23 +103,25 @@ public class BsjC
 				System.getProperty("java.class.path")));
 		
 		BsjFileManager bfm = new LocationMappedFileManager(map);
+
+		// handle paths
 		
-
-
-		BsjFileObject bfo = null;
-		try
+		// get files for compilation
+		List<BsjFileObject> sourceFiles = new ArrayList<BsjFileObject>();		
+		for (String sourceFile : args)
 		{
-			bfo = bfm.getJavaFileForInput(
-					BsjCompilerLocation.SOURCE_PATH, "BsjClass", Kind.SOURCE);
-		} catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			sourceFiles.add(
+					bfm.getJavaFileForInput(
+							BsjCompilerLocation.SOURCE_PATH, 
+							sourceFile.replaceFirst(".java", ""), 
+							Kind.SOURCE));
 		}
-
+		
+		
+		// compile
 		BsjC bsjc = new BsjC();
-		bsjc.bsjFileManager = bfm;
-		bsjc.compileObjects = Arrays.asList(bfo);
+		bsjc.setBsjFileManager(bfm);
+		bsjc.setCompileObjects(sourceFiles);
 		bsjc.compile();
 	}
 
@@ -102,6 +129,68 @@ public class BsjC
 	{
 		StandardBsjCompiler compiler = new StandardBsjCompiler(bsjFileManager);
 		compiler.compile(compileObjects, listener);
+	}
+	
+	public static Options initOptions()
+	{
+		Options options = new Options();
+		
+		options.addOption("cp", "classpath", true, 
+				"Specify where to find user class files and annotation processors");
+		options.addOption("d", true, "Specify where to place generated class files");
+		options.addOption("version", false, "Version information");
+		options.addOption("gsp", "gensourcepath", true, 
+			"Specify where to place BSJ generated files");
+		
+		return options;
+	}
+
+	/**
+	 * @return the bsjFileManager
+	 */
+	public BsjFileManager getBsjFileManager()
+	{
+		return bsjFileManager;
+	}
+
+	/**
+	 * @param bsjFileManager the bsjFileManager to set
+	 */
+	public void setBsjFileManager(BsjFileManager bsjFileManager)
+	{
+		this.bsjFileManager = bsjFileManager;
+	}
+
+	/**
+	 * @return the listener
+	 */
+	public DiagnosticListener<? super JavaFileObject> getListener()
+	{
+		return listener;
+	}
+
+	/**
+	 * @param listener the listener to set
+	 */
+	public void setListener(DiagnosticListener<? super JavaFileObject> listener)
+	{
+		this.listener = listener;
+	}
+
+	/**
+	 * @return the compileObjects
+	 */
+	public List<BsjFileObject> getCompileObjects()
+	{
+		return compileObjects;
+	}
+
+	/**
+	 * @param compileObjects the compileObjects to set
+	 */
+	public void setCompileObjects(List<BsjFileObject> compileObjects)
+	{
+		this.compileObjects = compileObjects;
 	}
 	
 }
