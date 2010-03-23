97716c86c6f49aba457390a1d4272a1283db09c9
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-03-23 17:01:41 +0000

    Removed invalid TODO.  (The internationalization utilities are implementation-specific and therefore do not belong on the toolkit.)

--- compiler/utils/src/edu/jhu/cs/bsj/compiler/impl/utils/i18n/InternationalizationUtilities.java
+++ compiler/utils/src/edu/jhu/cs/bsj/compiler/impl/utils/i18n/InternationalizationUtilities.java
@@ -6,7 +6,6 @@ package edu.jhu.cs.bsj.compiler.impl.utils.i18n;
  */
 public class InternationalizationUtilities
 {
-	// TODO: move this to the toolkit once we have a chance (and a toolkit actually exists)
 	/** The repository used for looking up BSJ compiler messages. */
 	public static final StringRepository MESSAGE_REPOSITORY = new PropertyBasedStringRepository("/resources/messages/");
 }
