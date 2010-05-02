de25659f9653619dad0f97c11c31d16f91691454
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-05-02 21:45:29 +0000

    Fix for JVMs that don't resolve double path separators in resource names

--- compiler/utils/src/edu/jhu/cs/bsj/compiler/impl/utils/Utilities.java
+++ compiler/utils/src/edu/jhu/cs/bsj/compiler/impl/utils/Utilities.java
@@ -4,14 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 /**
  * A class for (very) general utility functions.
+ * 
  * @author Zachary Palmer
  */
 public class Utilities
 {
+	/** The logger for the utilities operations. */
+	private static Logger LOGGER = Logger.getLogger(Utilities.class);
+
 	/**
 	 * Retrieves an enum from the given array with the specified name.
+	 * 
 	 * @param a The array of enums.
 	 * @param n The name.
 	 * @return The named enum or <code>null</code> if no such enum exists.
@@ -27,23 +34,46 @@ public class Utilities
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Retrieves a resource in Java properties file format from the classpath.
+	 * 
 	 * @param path The name of the resource.
 	 * @return The resulting {@link Properties} object or <code>null</code> if the resource could not be accessed.
 	 */
 	public static Properties loadProperties(String path)
 	{
+		// Sanitize pathname
+		path = path.replaceAll("//+","/");
+		if (LOGGER.isTraceEnabled())
+		{
+			LOGGER.trace("Attempting to load properties from classpath resource " + path);
+		}
 		try
 		{
 			InputStream is = Utilities.class.getResourceAsStream(path);
+			if (is == null)
+			{
+				if (LOGGER.isTraceEnabled())
+				{
+					LOGGER.trace("Could not find resource " + path);
+					return null;
+				}
+			}
 			Properties p = new Properties();
 			p.load(is);
 			is.close();
+			if (LOGGER.isTraceEnabled())
+			{
+				LOGGER.trace("Loaded properties file " + path + " as: " + p);
+			}
 			return p;
 		} catch (IOException e)
 		{
+			if (LOGGER.isTraceEnabled())
+			{
+				LOGGER.trace("Could not load resource " + path + ": " + e);
+			}
 			return null;
 		}
 	}
