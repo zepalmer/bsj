c080c21c7987dc15518898774c4801a1954a3a5a
Nathan Krasnopoler <Nathan Krasnopoler@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-08-25 20:51:55 +0000

    Made Metaannotations a separate project.

--- compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/compiler/metaannotation/MetaAnnotationBuilderMetaprogramTest.java
+++ compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/compiler/metaannotation/MetaAnnotationBuilderMetaprogramTest.java
@@ -1,8 +1,10 @@
-package edu.jhu.cs.bsj.tests.compiler.tool.compiler;
+package edu.jhu.cs.bsj.tests.compiler.tool.compiler.metaannotation;
 
 import java.io.File;
 
 import org.junit.Test;
+
+import edu.jhu.cs.bsj.tests.compiler.tool.compiler.AbstractBsjCompilerTest;
 
 /**
  * Tests some sources that make use of meta-annotation builder metaprograms.
