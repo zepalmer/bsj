715400fe2bef2fd985ac2ecd39dac20ea6e15dd8
Zachary Palmer <Zachary Palmer@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-08-30 14:49:53 +0000

    Added a mapping between assignment operators and the binary operators that they abbreviate.

--- compiler/api/src/edu/jhu/cs/bsj/compiler/ast/AssignmentOperator.java
+++ compiler/api/src/edu/jhu/cs/bsj/compiler/ast/AssignmentOperator.java
@@ -1,29 +1,51 @@
 package edu.jhu.cs.bsj.compiler.ast;
 
+import static edu.jhu.cs.bsj.compiler.ast.BinaryOperator.*;
+
 public enum AssignmentOperator
 {
 	/** Assignment operator representing the token "<tt>=</tt>". */
-	ASSIGNMENT,
+	ASSIGNMENT(null),
 	/** Assignment operator representing the token "<tt>+=</tt>". */
-	PLUS_ASSIGNMENT,
+	PLUS_ASSIGNMENT(PLUS),
 	/** Assignment operator representing the token "<tt>-=</tt>". */
-	MINUS_ASSIGNMENT,
+	MINUS_ASSIGNMENT(MINUS),
 	/** Assignment operator representing the token "<tt>*=</tt>". */
-	MULTIPLY_ASSIGNMENT,
+	MULTIPLY_ASSIGNMENT(MULTIPLY),
 	/** Assignment operator representing the token "<tt>/=</tt>". */
-	DIVIDE_ASSIGNMENT,
+	DIVIDE_ASSIGNMENT(DIVIDE),
 	/** Assignment operator representing the token "<tt>&=</tt>". */
-	AND_ASSIGNMENT,
+	AND_ASSIGNMENT(LOGICAL_AND),
 	/** Assignment operator representing the token "<tt>|=</tt>". */
-	OR_ASSIGNMENT,
+	OR_ASSIGNMENT(LOGICAL_OR),
 	/** Assignment operator representing the token "<tt>^=</tt>". */
-	XOR_ASSIGNMENT,
+	XOR_ASSIGNMENT(XOR),
 	/** Assignment operator representing the token "<tt>%=</tt>". */
-	MODULUS_ASSIGNMENT,
+	MODULUS_ASSIGNMENT(MODULUS),
 	/** Assignment operator representing the token "<tt>&lt;&lt;=</tt>". */
-	LEFT_SHIFT_ASSIGNMENT,
+	LEFT_SHIFT_ASSIGNMENT(LEFT_SHIFT),
 	/** Assignment operator representing the token "<tt>&gt;&gt;=</tt>". */
-	RIGHT_SHIFT_ASSIGNMENT,
+	RIGHT_SHIFT_ASSIGNMENT(RIGHT_SHIFT),
 	/** Assignment operator representing the token "<tt>&gt;&gt;&gt;=</tt>". */
-	UNSIGNED_RIGHT_SHIFT_ASSIGNMENT,
+	UNSIGNED_RIGHT_SHIFT_ASSIGNMENT(UNSIGNED_RIGHT_SHIFT),
+
+	;
+
+	/** The binary operator abbreviated by this assignment or <code>null</code> if no such operator exists. */
+	private BinaryOperator binaryOperator;
+
+	private AssignmentOperator(BinaryOperator binaryOperator)
+	{
+		this.binaryOperator = binaryOperator;
+	}
+
+	/**
+	 * Retrieves the binary operator abbreviated by this assignment.
+	 * 
+	 * @return The binary operator abbreviated by this assignment or <code>null</code> if no such operator exists.
+	 */
+	public BinaryOperator getBinaryOperator()
+	{
+		return binaryOperator;
+	}
 }
