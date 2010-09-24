20e9716865a64e401a62db2944cdd9c677ff0374
Luwei Zhou <Luwei Zhou@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-09-24 02:06:27 +0000

    editor setup

---
+++ compiler/plugin/src/edu/jhu/cs/bsj/plugin/eclipse/editor/BSJDocumentSetupParticipant.java
@@ -0,0 +1,14 @@
+package edu.jhu.cs.bsj.plugin.eclipse.editor;
+
+import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
+import org.eclipse.jface.text.IDocument;
+
+public class BSJDocumentSetupParticipant implements IDocumentSetupParticipant {
+
+	@Override
+	public void setup(IDocument document) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
