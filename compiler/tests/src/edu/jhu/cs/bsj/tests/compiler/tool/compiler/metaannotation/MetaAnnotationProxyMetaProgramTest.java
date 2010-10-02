e22f44539fe29908b1dcdbfa0e56cd25e3294df5
Uday Garikipati <Uday Garikipati@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-10-02 16:44:26 +0000

    commit of supplymentry files to test proxy and the junit test cases for proxy and unroll

--- compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/compiler/metaannotation/MetaAnnotationProxyMetaProgramTest.java
+++ compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/compiler/metaannotation/MetaAnnotationProxyMetaProgramTest.java
@@ -7,16 +7,18 @@ import org.junit.Test;
 import edu.jhu.cs.bsj.tests.compiler.tool.compiler.AbstractBsjCompilerTest;
 
 /**
- * Tests some sources that make use of meta-annotation unroll metaprograms.
+ * Tests some sources that make use of meta-annotation proxy metaprograms.
  * @author Uday Garikipati
  */
-public class MetaAnnotationUnrollMetaprogramTest extends AbstractBsjCompilerTest
-{
+public class MetaAnnotationProxyMetaProgramTest  extends AbstractBsjCompilerTest{
+
 	@Test
 	public void testBsjCompiler() throws Exception
 	{
-		
+					
 		performTest(new File(EXAMPLES + File.separator + "projects" + File.separator + "bsj-tests" + File.separator
-				+ "metaannotation-test"), "UnrollClass");
+				+ "metaannotation-test"), "com/bar/FooImpl","ProxyClass");
+			
 	}
+
 }
