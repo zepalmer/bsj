13d25792d642c25949c085ff76d0c45f020c4812
Uday Garikipati <Uday Garikipati@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-11-28 20:47:13 +0000

    test for Delegate meta program

---
+++ compiler/tests/src/edu/jhu/cs/bsj/tests/compiler/tool/compiler/metaannotation/MetaAnnotationDelegateMetaprogramTest.java
@@ -0,0 +1,17 @@
+package edu.jhu.cs.bsj.tests.compiler.tool.compiler.metaannotation;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import edu.jhu.cs.bsj.tests.compiler.tool.compiler.AbstractBsjCompilerTest;
+
+public class MetaAnnotationDelegateMetaprogramTest extends AbstractBsjCompilerTest {
+		@Test
+		public void testBsjCompiler() throws Exception
+		{
+			String s = EXAMPLES + File.separator + "projects" + File.separator + "bsj-tests" + File.separator
+			+ "Delegates";
+			performTest(new File(s), "mypackage/DelegateClass", "mypackage/Test");
+		}
+}
