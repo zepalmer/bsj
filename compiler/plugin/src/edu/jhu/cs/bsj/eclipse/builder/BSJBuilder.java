241b3923c4e1b6dab9ded628d5019d1b845015e6
Luwei Zhou <Luwei Zhou@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-12-06 03:00:21 +0000

    bsj builder and nature template

---
+++ compiler/plugin/src/edu/jhu/cs/bsj/eclipse/builder/BSJBuilder.java
@@ -0,0 +1,19 @@
+package edu.jhu.cs.bsj.eclipse.builder;
+
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class BSJBuilder extends IncrementalProjectBuilder {
+
+	@Override
+	protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
+			throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
