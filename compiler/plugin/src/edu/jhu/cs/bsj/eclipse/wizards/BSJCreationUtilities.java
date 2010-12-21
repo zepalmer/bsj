898bfca57b62af36fba068799316716ef44147a9
Luwei Zhou <Luwei Zhou@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-12-21 13:39:16 +0000

    change perspective after project creation

---
+++ compiler/plugin/src/edu/jhu/cs/bsj/eclipse/wizards/BSJCreationUtilities.java
@@ -0,0 +1,9 @@
+package edu.jhu.cs.bsj.eclipse.wizards;
+
+/**
+ * Assist creating BSJ projects and files
+ */
+public class BSJCreationUtilities {
+	private BSJCreationUtilities() {
+	}
+}
--- compiler/plugin/src/edu/jhu/cs/bsj/eclipse/wizards/NewBSJProjectWizard.java
+++ compiler/plugin/src/edu/jhu/cs/bsj/eclipse/wizards/NewBSJProjectWizard.java
@@ -1,26 +1,56 @@
 package edu.jhu.cs.bsj.eclipse.wizards;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
-public class NewBSJProjectWizard extends Wizard implements INewWizard {
-
+public class NewBSJProjectWizard extends Wizard implements INewWizard, IExecutableExtension {
+	
+	private static final String WIZARD_NAME = "New BSJ Project";
+	private static final String PAGE_ONE_NAME = "Creating a BSJ Project.";
+	private static final String PAGE_ONE_TITLE = "Create a BSJ Project."; 
+	private WizardNewProjectCreationPage pageOne;
+	
+	private IConfigurationElement configurationElement;
+	
 	public NewBSJProjectWizard() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		// TODO Auto-generated method stub
-
+	}
+	
+	@Override
+	public void addPages() {
+		super.addPages();
+		
+		setWindowTitle(WIZARD_NAME);
+		
+		pageOne = new WizardNewProjectCreationPage(PAGE_ONE_NAME);
+		pageOne.setTitle(PAGE_ONE_TITLE);
+		pageOne.setDescription("Enter a project name");
+		addPage(pageOne);
 	}
 
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
+		
+		// change to BSJ perspective
+		BasicNewProjectResourceWizard.updatePerspective(configurationElement);
+		
+		return true;
 	}
-
+	
+	@Override
+	public void setInitializationData(IConfigurationElement config,
+			String propertyName, Object data) throws CoreException {
+		// retrieve configuration data needed for changing perspective
+		configurationElement = config;
+	}
 }
