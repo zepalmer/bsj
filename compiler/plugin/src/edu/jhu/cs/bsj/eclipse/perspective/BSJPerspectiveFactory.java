2b81a6cba239c3781d54606130db0807f8d9792e
Luwei Zhou <Luwei Zhou@6cffb945-13a6-4b8f-85cd-7c6f19cefdc8>
2010-12-21 04:28:21 +0000

    perspective layout

--- compiler/plugin/src/edu/jhu/cs/bsj/eclipse/perspective/BSJPerspectiveFactory.java
+++ compiler/plugin/src/edu/jhu/cs/bsj/eclipse/perspective/BSJPerspectiveFactory.java
@@ -1,14 +1,29 @@
 package edu.jhu.cs.bsj.eclipse.perspective;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class BSJPerspectiveFactory implements IPerspectiveFactory {
 
+	final static String ID_CONSOLE ="org.eclipse.ui.console.ConsoleView";
+	
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		// TODO Auto-generated method stub
-
+		 String editorArea = layout.getEditorArea();
+		 
+		 IFolderLayout left = layout.createFolder(
+				 "left", IPageLayout.LEFT, 0.25f, editorArea);
+		 left.addView(IPageLayout.ID_PROJECT_EXPLORER);
+		 
+		 IFolderLayout middleBottom = layout.createFolder(
+				 "middleBottom", IPageLayout.BOTTOM, 0.75f, editorArea);
+		 middleBottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+		 middleBottom.addView(ID_CONSOLE);
+		 
+		 IFolderLayout right = layout.createFolder(
+				 "right", IPageLayout.RIGHT, 0.75f, editorArea);
+		 right.addView(IPageLayout.ID_OUTLINE);
 	}
-
+	
 }
